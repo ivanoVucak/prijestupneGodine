e3f6d0901865e2e4bffd452687ae2fab455400b0
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-11-08 08:51:29 +0100

    Add files via upload

---
+++ provjeravanjePrijestupnihGodina/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeravanjePrijestupnihGodina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godina = 1900;
+
+
+            do {
+
+                if (godina % 4 == 0 && godina % 100 != 0 || godina % 400 == 0)
+                {
+                    Console.WriteLine(godina);
+                }
+
+                godina++;
+
+            }while (godina < 2001);
+
+            Console.ReadKey();
+        }
+    }
+}
